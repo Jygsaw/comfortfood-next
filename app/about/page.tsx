8b9bd7fe3ac04d2346cecff70bde8d175f7332e9
Justin Cheung <jygsaw@gmail.com>
2023-11-28 12:17:17 -0800

    [NEW] Add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { type Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'About Us | Vizro',
+};
+
+const Page = () => (
+    <>
+        This is the about page.
+    </>
+);
+
+export default Page;

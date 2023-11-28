10c500092536caf87cfcbd041084989e8399ec72
Justin Cheung <jygsaw@gmail.com>
2023-11-27 17:25:57 -0800

    [NEW] Add Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Header = () => <header>Vizro</header>;
+
+export default Header;
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,10 @@
 import React, { type PropsWithChildren, type ReactNode } from 'react';
+import Header from 'components/Header';
 import Footer from 'components/Footer';
 
 const Layout = ({ children }: PropsWithChildren): ReactNode => (
     <>
+        <Header />
         <main>{children}</main>
         <Footer />
     </>

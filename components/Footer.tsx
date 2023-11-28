dd08830aa7cd4b7e1a90a3bc776da5d2714c5881
Justin Cheung <jygsaw@gmail.com>
2023-11-27 17:01:43 -0800

    [NEW] Add Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Footer = () => <footer>&copy; 2023 Justin Cheung. All rights reserved.</footer>;
+
+export default Footer;
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,11 @@
 import React, { type PropsWithChildren, type ReactNode } from 'react';
+import Footer from 'components/Footer';
 
-const Layout = ({ children }: PropsWithChildren): ReactNode => <main>{children}</main>;
+const Layout = ({ children }: PropsWithChildren): ReactNode => (
+    <>
+        <main>{children}</main>
+        <Footer />
+    </>
+);
 
 export default Layout;

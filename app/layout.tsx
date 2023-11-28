7d5a126c0fcec1d077958a399a334b53d3d72cda
Justin Cheung <jygsaw@gmail.com>
2023-11-28 12:11:41 -0800

    [CHG] Update root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
 import React, { type PropsWithChildren } from 'react';
 import Layout from 'components/Layout';
-
 import './globals.css';
 
 const RootLayout = ({ children }: PropsWithChildren) => (
     <html lang="en">
-        <head>
-            <title>Vizro Title</title>
-        </head>
+        <head />
         <body>
             <Layout>
                 {children}

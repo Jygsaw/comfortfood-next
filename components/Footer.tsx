26350a4e2a3948f2ba5a584455b4a12889da229d
Justin Cheung <jygsaw@gmail.com>
2023-11-28 12:17:33 -0800

    [CHG] Update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import Link from 'next/link';
 
-const Footer = () => <footer>&copy; 2023 Justin Cheung. All rights reserved.</footer>;
+const Footer = () => (
+    <footer>
+        &copy; 2023 Justin Cheung. All rights reserved.
+        <Link href="/about">About</Link>
+    </footer>
+);
 
 export default Footer;

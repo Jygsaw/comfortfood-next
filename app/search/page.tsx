3b0e9d73ebdf9a94bfc7a37bb37628519b3f1b74
Justin Cheung <jygsaw@gmail.com>
2023-11-29 08:14:49 -0800

    [NEW] Create search page

---
+++ app/search/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { titleAppend } from 'lib/seoUtils';
+import { type Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: titleAppend('Search'),
+};
+
+const Page = () => (
+    <>
+        <div>search bar</div>
+        <div>
+            <div>recipe card</div>
+            <div>recipe card</div>
+            <div>recipe card</div>
+        </div>
+        <div>
+            <div>recipe card</div>
+            <div>recipe card</div>
+            <div>recipe card</div>
+        </div>
+    </>
+);
+
+export default Page;

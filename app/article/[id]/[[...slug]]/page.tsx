d9fd9c583979040012065d3539bf75ea6f8484b8
Justin Cheung <jygsaw@gmail.com>
2023-11-29 09:55:30 -0800

    [NEW] Create article page

---
+++ app/article/[id]/[[...slug]]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Page = ({ params: { id, slug }  }: { params: { id: string, slug: string }}) => (
+    <>
+        Interesting article about a recipe
+    </>
+);
+
+export default Page;

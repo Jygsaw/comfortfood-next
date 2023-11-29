ec06a00706d1a0ef720e922e0ac4f701bc2a72b4
Justin Cheung <jygsaw@gmail.com>
2023-11-29 10:09:59 -0800

    [NEW] Create recipe page

---
+++ app/recipe/[id]/[[...slug]]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Page =({ params: { id, slug } }: { params: { id: string, slug: string}}) => (
+    <>
+        Amazing recipe for delicious food
+    </>
+);
+
+export default Page;

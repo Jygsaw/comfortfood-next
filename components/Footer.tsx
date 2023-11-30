05c8a03e4b97e858d918868a2cdcda05431e9ee7
Justin Cheung <jygsaw@gmail.com>
2023-11-30 13:59:12 -0800

    [CHG] Add footer css

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import Link from "next/link";
 
 const Footer = () => (
-    <footer>
-        &copy; 2023 Justin Cheung. All rights reserved.
-        <Link href="/about">About</Link>
+    <footer className="flex flex-col ">
+        <div className="flex justify-center gap-5">
+            <Link href="/about">About</Link>
+        </div>
+        <div className="flex justify-center text-gray-600">
+            &copy; 2023 Justin Cheung. All rights reserved.
+        </div>
     </footer>
 );
 

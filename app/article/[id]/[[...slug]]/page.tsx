84f5c8724f47572c5d4a147dfeaf3a3ea5b1efea
Justin Cheung <jygsaw@gmail.com>
2023-11-29 18:44:22 -0800

    [RFG] Update eslint rules

--- app/article/[id]/[[...slug]]/page.tsx
+++ app/article/[id]/[[...slug]]/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import type { dynamicRoute } from "types/routing";
 
-const Page = ({ params: { id, slug }  }: dynamicRoute) => (
-    <>
+const Page = ({ params: { id }  }: dynamicRoute) => {
+    console.log("fetching id:", id);
+    return (
+        <>
         Interesting article about a recipe
-    </>
-);
+        </>
+    );
+};
 
 export default Page;
--- app/recipe/[id]/[[...slug]]/page.tsx
+++ app/recipe/[id]/[[...slug]]/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import type { dynamicRoute } from "types/routing";
 
-const Page =({ params: { id, slug } }: dynamicRoute) => (
-    <>
-        Amazing recipe for delicious food
-    </>
-);
+const Page =({ params: { id } }: dynamicRoute) => {
+    console.log("fetching id:", id);
+    return     (
+        <>
+            Amazing recipe for delicious food
+        </>
+    );
+
+};
 
 export default Page;

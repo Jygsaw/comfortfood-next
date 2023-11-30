ac67e87ca14e2a29d30ed78f16fcc50ed0dfc85e
Justin Cheung <jygsaw@gmail.com>
2023-11-30 12:25:33 -0800

    [RFG] Return mock recipes based on query

--- lib/recipesAPI.ts
+++ lib/recipesAPI.ts
@@ -2,42 +2,8 @@ import type { Recipe } from "types/recipe";
 
 // TODO: create recipe API
 const RECIPE_API = "https://api.github.com/repos/Jygsaw/vizro-next";
-const RECIPES_1 = [
-    {
-        id: "1",
-        slug: "recipe-1",
-        name: "recipe 1"
-    },
-    {
-        id: "2",
-        slug: "recipe-2",
-        name: "recipe 2"
-    },
-    {
-        id: "3",
-        slug: "recipe-3",
-        name: "recipe 3"
-    },
-];
-const RECIPES_2 = [
-    {
-        id: "4",
-        slug: "recipe-4",
-        name: "recipe 4"
-    },
-    {
-        id: "5",
-        slug: "recipe-5",
-        name: "recipe 5"
-    },
-    {
-        id: "6",
-        slug: "recipe-6",
-        name: "recipe 6"
-    },
-];
 
-export async function getRecipes(): Promise<Recipe[]> {
+export async function getRecipes(query: string): Promise<Recipe[]> {
     const url = RECIPE_API;
 
     return fetch(url)
@@ -49,7 +15,16 @@ export async function getRecipes(): Promise<Recipe[]> {
             if (json.errors) throw new Error("Fetch API failed: " + url);
             return json.recipes || [];
         })
+        // TODO remove mock
         .then(() => {
-            return [...RECIPES_1, ...RECIPES_2];
+            const recipes = [];
+            for (let i = 0; i < (query?.length ?? 0); i++) {
+                recipes.push({
+                    id: i.toString(),
+                    slug: `recipe-${i}`,
+                    name: `recipe ${i}`,
+                });
+            }
+            return recipes;
         });
 };

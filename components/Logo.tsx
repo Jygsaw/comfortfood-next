d90dae3a7a283a88c9ac1a05f9d5867ab0554757
Justin Cheung <jygsaw@gmail.com>
2023-12-01 10:28:31 -0800

    [NEW] Create Logo

---
+++ components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+    return (
+        <div className="bg-orange-400 inline-block px-3 py-4 text-white text-3xl font-bold">
+            ComfortFood
+        </div>
+    );
+};
+
+export default Logo;

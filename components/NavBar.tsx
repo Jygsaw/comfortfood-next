faf9e074567f949cafca005ea5b807275f8c6d36
Justin Cheung <jygsaw@gmail.com>
2023-11-30 23:41:35 -0800

    [CHG] Add links to navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
-const NavBar = () => (
-    <nav className="flex">
-        <div>Home</div>
-        <div>Cookbook</div>
-        <div>Articles</div>
-        <div>Recipes</div>
-        <div>Search</div>
-    </nav>
-);
+const NavBar = () => {
+    return (
+        <nav className="flex gap-16 justify-center text-white text-2xl font-semibold">
+            <div><Link href="/">Home</Link></div>
+            <div><Link href="/cookbook">Cookbook</Link></div>
+            <div><Link href="/articles">Articles</Link></div>
+            <div><Link href="/recipes">Recipes</Link></div>
+            <div><Link href="/search">Search</Link></div>
+        </nav>
+    );
+};
 
 export default NavBar;

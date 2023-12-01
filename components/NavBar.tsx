a89d88577d56148bcc9b7551656d51952ec9e7d7
Justin Cheung <jygsaw@gmail.com>
2023-12-01 13:02:38 -0800

    [RFG] Remove unneeded navbar divs

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 const NavBar = () => {
     return (
         <nav className="flex gap-16 justify-center text-white text-2xl font-semibold">
-            <div><Link href="/">Home</Link></div>
-            <div><Link href="/cookbook">Cookbook</Link></div>
-            <div><Link href="/articles">Articles</Link></div>
-            <div><Link href="/recipes">Recipes</Link></div>
-            <div><Link href="/search">Search</Link></div>
+            <Link href="/">Home</Link>
+            <Link href="/cookbook">Cookbook</Link>
+            <Link href="/articles">Articles</Link>
+            <Link href="/recipes">Recipes</Link>
+            <Link href="/search">Search</Link>
         </nav>
     );
 };

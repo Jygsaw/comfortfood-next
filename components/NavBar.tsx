296ec87f8837b7641550f8627b8628797eae418e
Justin Cheung <jygsaw@gmail.com>
2023-11-30 19:48:07 -0800

    [NEW] Create NavBar

---
+++ components/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => (
+    <nav className="flex">
+        <div>Home</div>
+        <div>Cookbook</div>
+        <div>Articles</div>
+        <div>Recipes</div>
+        <div>Search</div>
+    </nav>
+);
+
+export default NavBar;

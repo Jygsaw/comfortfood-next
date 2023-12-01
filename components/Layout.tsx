a91d781ff8f9bcb3b2eee04e577536a6dc3b76aa
Justin Cheung <jygsaw@gmail.com>
2023-12-01 11:34:58 -0800

    [CHG] Update site layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,7 +5,7 @@ import type { PropsWithChildren, ReactNode } from "react";
 
 const Layout = ({ children }: PropsWithChildren): ReactNode => (
     <>
-        <div className="min-h-screen flex flex-col mx-20">
+        <div className="container max-w-screen-7xl min-h-screen mx-auto flex flex-col">
             <header>
                 <Header />
             </header>

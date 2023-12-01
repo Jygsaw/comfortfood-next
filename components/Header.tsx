f27cf55a20a99b459d41c09a873811c5a020d7db
Justin Cheung <jygsaw@gmail.com>
2023-12-01 10:30:52 -0800

    [CHG] Update header css

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import Link from "next/link";
-import NavBar from "components/NavBar";
 import AccountControls from "components/AccountControls";
+import Logo from "components/Logo";
+import NavBar from "components/NavBar";
 
 const Header = () => {
     return (
-        <div className="flex">
-            <div>
-                <Link href="/">Vizro</Link>
+        <div className="bg-slate-500 min-h-24 px-5 py-3 flex gap-7 items-center">
+            <div className="w-48 min-w-fit">
+                <Link href="/"><Logo /></Link>
             </div>
             <div className="flex-grow">
                 <NavBar />
             </div>
-            <div>
+            <div className="w-48 min-w-fit">
                 <AccountControls />
             </div>
         </div>

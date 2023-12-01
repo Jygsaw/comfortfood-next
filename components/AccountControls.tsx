4420abcc85d3c53732828bd4c4f2d53355e433fc
Justin Cheung <jygsaw@gmail.com>
2023-12-01 12:24:08 -0800

    [RFG] Switch to nav element

--- components/AccountControls.tsx
+++ components/AccountControls.tsx
@@ -5,11 +5,11 @@ const AccountControls = () => {
     const isLoggedIn = false;
 
     return (
-        <div className="flex gap-5 justify-end text-white text-xl font-semibold">
+        <nav className="flex gap-5 justify-end text-white text-xl font-semibold">
             {isLoggedIn && <Link href="/profile">Profile</Link>}
             {isLoggedIn && <Link href="/logout">Logout</Link>}
             {!isLoggedIn && <Link href="/login">Login</Link>}
-        </div>
+        </nav>
     );
 };
 
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 const Footer = () => {
     return (
         <div className="flex flex-col ">
-            <div className="flex justify-center gap-5">
+            <nav className="flex justify-center gap-5">
                 <Link href="/about">About</Link>
-            </div>
+            </nav>
             <div className="flex justify-center text-gray-600">
             &copy; 2023 Justin Cheung. All rights reserved.
             </div>

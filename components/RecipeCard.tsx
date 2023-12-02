5cf841c1af1681b6532be010f6d09066634ad494
Justin Cheung <jygsaw@gmail.com>
2023-12-01 16:29:06 -0800

    [CHG] Implement RecipeCard

--- components/RecipeCard.tsx
+++ components/RecipeCard.tsx
@@ -1,19 +1,29 @@
 import React from "react";
+import Image from "next/image";
 import Link from "next/link";
+import type { Recipe } from "types/recipe";
 
-type Input = {
-    id: string,
-    slug?: string,
-}
-
-const RecipeCard = ({ id, slug }: Input) => {
+const RecipeCard = ({ id, slug, name, image, description, likes, bookmarked }: Recipe) => {
     const href = `recipe/${id}/${slug}`;
+
     return (
-        <div>
-            <Link href={href}>
-                RecipeCard
+        <article className="relative w-full h-full border-2 shadow-md">
+            <Link className="group" href={href}>
+                <Image src={image} alt={name} fill />
+                <section className="absolute w-full h-full flex flex-col z-10 bg-gray-800 bg-opacity-0 hover:bg-opacity-80">
+                    <header className="h-12">
+                        {bookmarked && <span className="absolute top-0 right-0">BKMRK</span>}
+                    </header>
+                    <p className="flex-grow m-3 text-white invisible group-hover:visible">
+                        {description}
+                    </p>
+                    <footer className="flex h-16 place-items-end bg-gradient-to-t from-gray-800 to-transparent">
+                        <p className="flex-grow mx-2 my-1 text-white text-lg truncate">{name}</p>
+                        <span className="mx-2 my-1 text-white text-lg">LIKE:{likes}</span>
+                    </footer>
+                </section>
             </Link>
-        </div>
+        </article>
     );
 };
 

af92f1d124e664349cb8878aae30b1a967cc10c3
Justin Cheung <jygsaw@gmail.com>
2023-12-01 21:35:18 -0800

    [CHG] Update next with external image pattern

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
     eslint: {
         dirs: ["/"],
     },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "d2lnr5mha7bycj.cloudfront.net",
+                port: "",
+                pathname: "/product-image/file/large_59565821-77e2-437c-9113-cdf304316dd4.jpg",
+            },
+        ],
+    },
 
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     // trailingSlash: true,

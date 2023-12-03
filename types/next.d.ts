d2b82bb67afb9377e3e665d6193fd75b1b5ce258
Justin Cheung <jygsaw@gmail.com>
2023-12-02 21:25:10 -0800

    [NEW] Add PageProps type

---
+++ types/next.d.ts
@@ -0,0 +1,6 @@
+// TODO: find a better official source
+// note: interface based on ".next\types\app\page.ts"
+export interface PageProps {
+    params?: Record<string, string>,
+    searchParams?: Record<string, string>,
+}

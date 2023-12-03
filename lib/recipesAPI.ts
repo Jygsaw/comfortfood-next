cf9cf1f4f7082e1880dc48795c6696e80436b1d3
Justin Cheung <jygsaw@gmail.com>
2023-12-03 01:51:43 -0800

    [NEW] Integrate HOSTNAME env var

--- lib/recipesAPI.ts
+++ lib/recipesAPI.ts
@@ -1,10 +1,9 @@
 import type { Recipe } from "types/recipe";
 
-// TODO: replace hardcoded hostname with environment variables
-const RECIPE_API = "http://localhost:3000/api/recipes";
+const RECIPE_API = "/api/recipes";
 
 export async function getRecipes(searchParams: URLSearchParams): Promise<Recipe[]> {
-    const url = `${RECIPE_API}?${searchParams.toString()}`;
+    const url = `http://${process.env.HOSTNAME}${RECIPE_API}?${searchParams.toString()}`;
 
     return fetch(url)
         .then(response => {

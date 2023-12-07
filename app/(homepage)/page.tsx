e31ba3ae0bbda78d9b31ecfc0a6b97cd4a855812
Justin Cheung <jygsaw@gmail.com>
2023-12-07 01:52:15 -0800

    [CHG] Update homepage layout

--- app/(homepage)/page.tsx
+++ app/(homepage)/page.tsx
@@ -1,24 +1,45 @@
 import React from "react";
 import { titleAppend } from "app/_lib/siteUtils";
 import UnderConstruction from "app/_components/UnderConstruction";
+import Teaser from "app/_components/Teaser";
 import type { Metadata } from "next";
+import { generateRecipes } from "app/_lib/testUtils";
 
 export const metadata: Metadata = {
     title: titleAppend("Homepage"),
 };
 
+// TODO: refine thoughts on data models and storage
+const teaserData = [
+    {
+        title: "Seasonal Favorites",
+        link: "/articles",
+        cardData: generateRecipes(3),
+    },
+    {
+        title: "Easy Snacks",
+        link: "/recipes",
+        cardData: generateRecipes(3),
+    },
+    {
+        title: "Tips and Tricks",
+        link: "/articles",
+        cardData: generateRecipes(3),
+    },
+];
+
 const Page = () => {
     return (
-        <>
-            <section className="relative w-full h-[calc(100vh-92px)] mx-auto">
+        <div className="bg-orange-50">
+            <section className="relative w-full h-[calc(100vh-92px)] mx-auto bg-gradient-to-b from-white to-orange-50">
                 <UnderConstruction />
             </section>
-            <section className="container mx-auto flex flex-col" >
-                <div>SOMETHING</div>
-                <div>SOMETHING</div>
-                <div>SOMETHING</div>
-            </section>
-        </>
+            {teaserData.map(data => (
+                <div key={data.title} className="w-full my-12 last:bg-gradient-to-b last:from-orange-50 last:to-white">
+                    <Teaser {...data} />
+                </div>
+            ))}
+        </div>
     );
 };
 

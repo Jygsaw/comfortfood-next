7d660e0362a69c5bf74465c8f146c3389cf35367
Justin Cheung <jygsaw@gmail.com>
2023-12-07 09:40:20 -0800

    [CHG] Add buildUrl to site utils

--- app/_lib/siteUtils.ts
+++ app/_lib/siteUtils.ts
@@ -1,3 +1,5 @@
+import { UrlSearchParams } from "app/_types/search";
+
 import type { DataType } from "app/_types/card";
 
 export function titleAppend(title: string) {
@@ -6,9 +8,17 @@ export function titleAppend(title: string) {
 
 export function buildPath(type: DataType, id: string | number, slug: string) {
     switch (type) {
-    case "recipe":
-        return `recipe/${id}/${slug}`;
-    default:
-        throw new Error(`Unknown path type: ${type}`);
+        case "recipe":
+            return `/recipe/${id}/${slug}`;
+        default:
+            throw new Error(`Unknown path type: ${type}`);
     }
+}
+
+export function buildUrl(path: string, searchParams?: UrlSearchParams) {
+    const protocol = process.env.NEXT_PUBLIC_PROTOCOL;
+    const hostname = process.env.NEXT_PUBLIC_HOSTNAME;
+    const query = new URLSearchParams(searchParams).toString();
+
+    return `${protocol}${hostname}${path}${query && `?${query}`}`;
 }

569905bbfde20bead45a7e297bd1cd789e053030
Justin Cheung <jygsaw@gmail.com>
2023-12-07 09:27:42 -0800

    [CHG] Add generateRecipe override option

--- app/_lib/testUtils.ts
+++ app/_lib/testUtils.ts
@@ -1,8 +1,10 @@
+import type { Recipe } from "app/_types/recipe";
+
 export function generateRecipes(num: number) {
     return new Array(num).fill(null).map(() => generateRecipe());
 }
 
-export function generateRecipe() {
+export function generateRecipe(overrides?: Partial<Recipe>) {
     const id = Math.floor(Math.random() * 1000);
     return {
         type: "recipe",
@@ -13,5 +15,6 @@ export function generateRecipe() {
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         likes: Math.floor(Math.random() * 10),
         bookmarked: Math.random() < 0.5,
+        ...overrides,
     } as const;
 }
--- app/api/recipes/route.ts
+++ app/api/recipes/route.ts
@@ -1,11 +1,12 @@
 import { generateRecipe } from "app/_lib/testUtils";
+
 import type { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
     const q = request.nextUrl.searchParams.get("q") ?? "";
     const recipes = new Array(q.length)
         .fill(null)
-        .map((_, i) => generateRecipe(i.toString()));
+        .map(() => generateRecipe());
 
     return Response.json({ data: { recipes } });
 }

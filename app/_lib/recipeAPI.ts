dcc6442e120d066d942fb3f5ffcc8eab446c25da
Justin Cheung <jygsaw@gmail.com>
2023-12-07 06:50:38 -0800

    [NEW] Create recipe api

---
+++ app/_lib/recipeAPI.ts
@@ -0,0 +1,21 @@
+import { buildUrl } from "./siteUtils";
+
+import type { Recipe } from "app/_types/recipe";
+
+const RECIPE_API = "/api/recipe";
+
+export async function addRecipe(recipe: Recipe): Promise<Recipe> {
+    const url = buildUrl(RECIPE_API);
+    const options = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(recipe),
+    };
+
+    return fetch(url, options)
+        .then(response => {
+            if (!response.ok) throw new Error(response.statusText);
+            return response.json();
+        })
+        .then(json => json.data.recipe);
+}

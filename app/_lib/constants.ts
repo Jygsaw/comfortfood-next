61d6f0fced21fa06c2fd2c0aae24a9c6ccc26c3d
Justin Cheung <jygsaw@gmail.com>
2023-12-07 18:02:15 -0800

    [NEW] Create constants file

---
+++ app/_lib/constants.ts
@@ -0,0 +1,10 @@
+export const NEW_RECIPE = {
+    type: "recipe",
+    id: "NEW",
+    slug: "",
+    name: "",
+    image: "",
+    description: "",
+    likes: 0,
+    bookmarked: false,
+} as const;
--- app/_types/recipe.d.ts
+++ app/_types/recipe.d.ts
@@ -1,4 +1,5 @@
 export type Recipe = {
+    type: "recipe",
     id: string,
     slug: string,
     name: string,

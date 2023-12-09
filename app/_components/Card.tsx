3c40d44c5708b8a97b21c0af6401451a801d77d1
Justin Cheung <jygsaw@gmail.com>
2023-12-08 21:36:22 -0800

    [CHG] Update Card layout

--- app/_components/Card.tsx
+++ app/_components/Card.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { buildPath } from "app/_lib/siteUtils";
+
 import type { CardData } from "app/_types/card";
 
 const Card = ({ type, id, slug, name, imageLink, bookmarked }: CardData) => {
@@ -10,9 +11,9 @@ const Card = ({ type, id, slug, name, imageLink, bookmarked }: CardData) => {
     return (
         <Link className="w-full h-full" href={href}>
             <article className="w-full h-full flex flex-col bg-white hover:shadow-md">
-                <div className="relative w-full h-64 mx-auto">
+                <div className="relative w-full aspect-[1.5]">
                     {bookmarked && <span className="absolute top-0 right-0 z-10">BKMRK</span>}
-                    <Image src={imageLink} alt={name} fill style={{ objectFit: "contain" }} />
+                    <Image src={imageLink} alt={name} fill style={{ objectFit: "cover" }} />
                 </div>
                 <p className="mx-2 my-1 flex-grow text-lg">
                     {name}

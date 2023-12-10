3ee6ed8f6bfb3dee362fe511c64c3b5a6abd15af
Justin Cheung <jygsaw@gmail.com>
2023-12-10 08:45:51 -0800

    [NEW] Create generateArticle test util

---
+++ app/_lib/mockArticle.ts
@@ -0,0 +1,149 @@
+export const MOCK_ARTICLE = {
+    name: "What's the Difference Between Expiration, Best-By, Sell-By, and Use-By Dates?",
+    description: "Reducing food waste is critical. Understanding expiration, sell-by, best-by, use-by, and freeze-by date labels can help you do your part, as well as save you money.",
+    createdBy: "SU-JIT LIN",
+    createdAt: "April 08, 2022",
+    updatedAt: "April 08, 2022",
+    content: `
+        <image src="https://www.simplyrecipes.com/thmb/aNnI7QfPFNrPdzU0Mbb7EcRvN4I=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Expiration-Dates-LEAD-a9c08d6e07f14ea1a80e06e1dd86cdd7.jpg" alt="" />
+        <nav>
+        <h2>
+        IN THIS ARTICLE
+        </h2>
+        <link>
+        Why do we have expiration dates, sell-by dates, use-by dates, and best-by dates?
+        </link>
+        <link>
+        What are expiration dates?
+        </link>
+        <link>
+        What are best-by and use-by dates?
+        </link>
+        <link>
+        What are sell-by dates?
+        </link>
+        <link>
+        What about the other dates that may be printed on the food label?
+        </link>
+        <link>
+        Is expired food safe to eat? Can I get sick from eating expired food?
+        </link>
+        </nav>
+        <p>
+        This post has been reviewed and approved by Katie Morford, RD
+        </p>
+        <p>
+        Food waste is a tremendous problem here in the “Land of Plenty,” a moniker that’s becoming less accurate as supply chain issues continue to impact everything we use, cook, drink, and eat. According to Feeding America, during a normal year, 108 billion pounds of food is wasted in the United States—an embarrassing 40% of all the food created in this country. While this number includes what’s thrown away at restaurants and grocery stores, as well as food tossed by farmers, packers, and manufacturers, there’s a lot discarded unnecessarily at home, too. In fact, an estimated 39% of all food waste is lost or wasted at the household level—and often simply because printed dates, which are voluntarily designated, are misinterpreted.
+        </p>
+        <p>
+        Now with shortages to consider, it’s more important than ever to ask, what is an expiration date? What’s the difference between that and a sell-by date? What about best-by dates? A healthy understanding of the necessity (or frivolity!) of any of these labels can do a great deal in helping you contribute to decreasing food waste—and getting the most out of every grocery dollar spent.
+        </p>
+        <p>
+        Why do we have expiration dates, sell-by dates, use-by dates, and best-by dates?
+        </p>
+        <p>
+        A common misconception is that all of these categories of dates exist by federal regulation, manufacturer/producer guidelines, or just general law. The truth is they don’t. They’re not required by the USDA, nor are they closely monitored by any governmental agency, unless it pertains to infant formula.
+        </p>
+        <p>
+        So then why bother?
+        </p>
+        <p>
+        There are a few reasons makers include these dates on their packaging, outside of the fact that we Americans have grown accustomed to looking at and for them. There’s the obvious and capitalistic reason that an earlier date means a greater likelihood of requiring replacement, and therefore, another sale. The other primary rationale for including such a date is to leave consumers with the best impression of their product, which would be within the window of time as printed on the label.
+        </p>
+        <p>
+        They’re also helpful for supermarkets and grocery stores, to regulate, circulate, and merchandise their product assortment. The inclusion of these labels provides them with the opportunity to cycle out their older inventory by putting those items more forward on displays, increasing the likelihood of their being purchased sooner than the ones that arrived most recently. This maximizes the shelf life of the display with less waste for the store.
+        </p>
+        <h2>
+
+        What are expiration dates?
+        </h2>
+        <p>
+        The use of the “expiration date” label has waned in favor of the more accurate–and less alarming–"best by" and "sell by" labeling. They’re now often reserved for items where usage after the printed date is strongly not recommended, as nutrition, efficacy, and potency can be affected.
+        </p>
+        <p>
+
+        The most critical example is infant formula, which is the only food item required by law to display and adhere to a labeled date. This is because its nutritional profile is developed with extreme precision, and those nutrients may deteriorate over time. However, both stores and you have a year from manufacture to sell and use it, respectively, so it’s typically not difficult to follow this recommendation.
+        </p>
+        <p>
+
+        Other items that may use the terminology “expiration date” include other baby foods, vitamins, over-the-counter medications, cake mix, baking powder, yeast, and pectin. All except the first simply won’t behave the way it’s required beyond a certain date, becoming less effective over time. If you want your dough to rise, your jam to gel, and your vitamins to be at their potent peak, take a look at the date to make sure it’s primed to perform.
+        </p>
+
+        <img src="https://www.simplyrecipes.com/thmb/cQ6cV2mVF9r6lwSVt2g9W9SK-wE=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-HT-Store-Spices-LEAD-2-d9820881e0c948e488192fe13fe04b5b.jpg" alt="" />
+
+        <h2>
+
+        What are best-by and use-by dates?
+        </h2>
+        <p>
+        The full phrase for this goes “Best if used by,” making the two parts somewhat interchangeable. It’s also the most transparent, honest label out there. It means exactly that: the product will taste, smell, look, and act best if used before this date. After that, the manufacturer makes no guarantees, as taste may weaken, items dry out, or staleness sets in. Essentially, it won’t be bad or inedible. it just won’t be at its best.
+        </p>
+        <p>
+
+        This label is common for items with pack dates listed, which helps stores and factories keep track of and rotate their products. You’ll most often see this one on classic non-perishables such as canned or aseptic (TetraPak) items or dry goods like crackers, cookies, herbs, and spices.
+        </p>
+        <p>
+
+        You might also see “Use or freeze by” on perishable items, such as meat. This indicates the window you have to preserve its peak quality, and has no bearing on safety nor purchase date, per the USDA.
+        </p>
+        <h2>
+
+        What are sell-by dates?
+        </h2>
+        <p>
+        Of all the categories, sell-by dates are most useful on the retail side. It provides retailers the chance to strategically circulate their inventory and display their items in an order that would encourage faster sale of older shipments. Instinctually, shoppers reach for the item at the front of the shelf instead of rifling through to the back. By identifying the products that need to be sold sooner in their best condition and putting those closer to hand, they reduce the need to throw out or order more of the food owing to misinterpretation of that date. Additionally, it allows the store to know when it’s time to reorder their stock.
+        </p>
+        <p>
+
+        Sell-by labels are the preferred term for most dairy, greens, and cold cuts. However, you can absolutely consume those items well beyond these dates. After all, they don’t expect you to consume your food the day it’s sold!
+        </p>
+        <p>
+
+        A week is usually the cap for lunch meat, but you have considerably longer for vacuum-sealed cold cuts. Milk, yogurt, cream, sour cream, cottage cheese and the like will depend on handling—how long it’s out of refrigeration, and when you open it, how long you expose it to possible spoilers like mold. Eggs, on the other hand, are safe for as long as five weeks after the printed date, if not longer.
+        </p>
+
+        <img src="https://www.simplyrecipes.com/thmb/HqKMGxc0PuEkscWb9jGA696qmc8=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__10__Peanut-Butter-Roundup-LEAD-03-b72485b163b748f2962d437f8e9ed44d.jpg" alt="" />
+
+        <h2>
+
+        What about the other dates that may be printed on the food label?
+        </h2>
+        <p>
+        For this, you may be referring to the closed dating code, which is applied by manufacturers to identify the date and time of production, particularly on shelf-stable products such as canned or boxed food.
+        </p>
+        <p>
+
+        While this series of digits and sometimes letters is typically very important to food producers in the case of quality assurance, you can usually ignore it. You’ll be asked for it in any customer service complaint, such as when shrimp was found in a box of Cinnamon Toast Crunch, but it has no relation to a best-by recommendation. In an extreme example, it’s what also helps them (and you!) track if the bag of salad you bought was part of a contaminated batch that may have come into contact with bacteria such as E. coli.
+        </p>
+        <p>
+
+        Another set of dates you might find are “pack dates.” As mentioned before, this may be on several types of non-perishables. Specifically, though, any USDA-graded egg carton is obligated to display this so that the consumer knows when the eggs were washed, graded, and–you guessed it!–packed. While in some states, a sell by or expiration date may be present as well, only the pack date is required.
+        </p>
+        <p>
+
+        Finally, you may also find Quality Assurance dates on items with long shelf lives, such as pasta, cereal, boxed soup, peanut butter, and condiments such as mustard and mayonnaise. This is a variation of “best if used by,” as it means they won’t stand by any satisfaction guarantee of their product beyond what’s stamped on the package. It’s their disclosure that quality, flavor, texture, and potency may deteriorate over time. However, you can go ahead and consume it with no worry provided it passes the tests we talk about next.
+        </p>
+        <h2>
+
+        Is expired food safe to eat? Can I get sick from eating expired food?
+        </h2>
+        <p>
+        While food is sealed, undamaged, and kept to an appropriate temperature, expired products are typically safe to eat. But as dubious as it sounds, your best bet is actually to do an old-fashioned sniff test, followed by a taste test if it passes the first criteria. Odors are the first sign of spoilage and an indicator of naturally occurring microorganisms–such as mold, bacteria, and yeast–breaking down the perishable item. These may cause the food to taste unpleasant, but are typically harmless in small quantities. Our recommendation is that you toss and desist if it fails both criteria, and definitely if it’s also changed in texture. A placebo effect can be just as sickening as an actual “bad egg,” as the term goes!
+        </p>
+        <p>
+
+        On the other hand, pathogenic bacteria, whose presence is transmitted via mishandling and contamination, can cause illness and stomach upset. Unlike spoilage bacteria, though, these harmful miscreants don’t affect the look, smell, or taste of food.
+        </p>
+        <p>
+
+        So while you may feel unwell from eating food past its prime due to an off flavor or aroma, you won’t get food poisoning from it unless it’s come into contact with one of the eight known ne'er-do-well cells: salmonella, C. perfrigens, E. coli, campylobacter, staph, listeria, toxoplasma, or common norovirus.
+        </p>
+        <p>
+
+        Pregnant women, young children, the elderly, and those with a compromised immune system should err on the side of caution. When in doubt, throw it out.
+        </p>
+        <p>
+        In summation, the most important thing is that your food is handled properly. Keep refrigerated items consistently chilled, don’t leave perishables and/or meats out for more than two hours, don’t let raw protein come into contact with items you’ll eat raw (that’s cross-contamination!), and use your best judgment. Printed dates may be written in ink and not pencil, but they’re certainly not written in stone.
+        </p>
+    `,
+};
--- app/_lib/testUtils.ts
+++ app/_lib/testUtils.ts
@@ -1,21 +1,40 @@
-import { NEW_RECIPE } from "app/_lib/constants";
+import { v4 as uuidv4 } from "uuid";
+import { NEW_ARTICLE, NEW_RECIPE } from "app/_lib/constants";
+import { MOCK_ARTICLE } from "app/_lib/mockArticle";
 
-import type { Recipe } from "app/_types/record";
+import type { Article, Recipe } from "app/_types/record";
 
 export function generateRecipes(num: number) {
     return new Array(num).fill(null).map(() => generateRecipe());
 }
 
 export function generateRecipe(overrides?: Partial<Recipe>) {
-    const id = Math.floor(Math.random() * 1000);
+    const id = uuidv4();
     return {
         ...NEW_RECIPE,
-        id: id.toString(),
+        id,
         slug: `lorem-ipsum-${id}`,
-        name: id % 3 ? `Lorem Ipsum ${id}` : `Lorem Ipsum ${id} Lorem ipsum dolor sit amet, consectetur adipiscing elit`,
+        name: Math.random() < 0.3 ? `Lorem Ipsum ${id}` : `Lorem Ipsum ${id} Lorem ipsum dolor sit amet, consectetur adipiscing elit`,
         imageLink: "https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_59565821-77e2-437c-9113-cdf304316dd4.jpg",
-        description: id % 2 ? "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        description: Math.random() < 0.5 ? "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         bookmarked: Math.random() < 0.5,
         ...overrides,
     } as const;
+}
+
+export function generateArticles(num: number) {
+    return new Array(num).fill(null).map(() => generateArticle());
+}
+
+export function generateArticle(overrides?: Partial<Article>) {
+    const id = uuidv4();
+    return {
+        ...NEW_ARTICLE,
+        id,
+        slug: `slug-${id}`,
+        imageLink: "https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_59565821-77e2-437c-9113-cdf304316dd4.jpg",
+        bookmarked: Math.random() < 0.5,
+        ...MOCK_ARTICLE,
+        ...overrides,
+    };
 }

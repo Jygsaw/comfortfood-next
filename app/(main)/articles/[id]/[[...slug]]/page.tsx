7bff69f7f684b323b0dca291837b56cea8c37347
Justin Cheung <jygsaw@gmail.com>
2023-12-11 14:37:23 -0800

    [CHG] Update mock article

--- app/(main)/articles/[id]/[[...slug]]/page.tsx
+++ app/(main)/articles/[id]/[[...slug]]/page.tsx
@@ -7,12 +7,14 @@ const Page = async ({ params: { id } }: DynamicRoute) => {
     const article = await getArticle(id);
 
     return (
-        <article className="m-12">
-            <h1>{article.name}</h1>
-            <p>{article.description}</p>
-            <p>{article.createdBy}</p>
-            <p>{article.updatedAt}</p>
-            <div dangerouslySetInnerHTML={{ __html: article.content }} />
+        <article className="max-w-4xl mx-auto my-12">
+            <h1 className="mb-2 text-4xl">{article.name}</h1>
+            <p className="mb-4 text-2xl text-gray-600">{article.description}</p>
+            <div className="my-4">
+                <p className="text-small">By {article.createdBy}</p>
+                <p className="text-small">Updated {article.updatedAt}</p>
+            </div>
+            <div className="my-6" dangerouslySetInnerHTML={{ __html: article.content }} />
         </article>
     );
 };

778b5b3a831e980c9bfeabfd47660fc9acaaf931
Justin Cheung <jygsaw@gmail.com>
2023-12-12 19:57:34 -0800

    [CHG] Update recipes api with draft functions

--- app/_lib/recipesAPI.ts
+++ app/_lib/recipesAPI.ts
@@ -30,15 +30,37 @@ export async function getRecipe(id: string): Promise<Recipe> {
             if (!response.ok) throw new Error(response.statusText);
             return response.json();
         })
-        .then(json =>json.data.recipe);
+        .then(json => json.data.recipe);
 }
 
-export async function createRecipe(data: Partial<Recipe>): Promise<Recipe> {
-    const url = buildUrl(RECIPES_API);
+export async function deleteRecipe(id: string): Promise<void> {
+    const url = buildUrl(`${RECIPES_API}/${id}`);
+    const options = {
+        method: "DELETE",
+    };
+
+    return fetch(url, options)
+        .then(response => {
+            if (!response.ok) throw new Error(response.statusText);
+        });
+}
+
+export async function createRecipeDraft(id: string = ""): Promise<void> {
+    const url = buildUrl(`${RECIPES_API}/${id}`);
     const options = {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data),
+    };
+
+    return fetch(url, options)
+        .then(response => {
+            if (!response.ok) throw new Error(response.statusText);
+        });
+}
+
+export async function getRecipeDraft(id: string): Promise<Recipe> {
+    const url = buildUrl(`${RECIPES_API}/${id}/draft`);
+    const options = {
+        method: "GET",
     };
 
     return fetch(url, options)
@@ -49,8 +71,8 @@ export async function createRecipe(data: Partial<Recipe>): Promise<Recipe> {
         .then(json => json.data.recipe);
 }
 
-export async function updateRecipe(id: string, data: Partial<Recipe>): Promise<Recipe> {
-    const url = buildUrl(`${RECIPES_API}/${id}`);
+export async function updateRecipeDraft(id: string, data: Partial<Recipe>): Promise<Recipe> {
+    const url = buildUrl(`${RECIPES_API}/${id}/draft`);
     const options = {
         method: "PATCH",
         headers: { "Content-Type": "application/json" },
@@ -65,10 +87,22 @@ export async function updateRecipe(id: string, data: Partial<Recipe>): Promise<R
         .then(json => json.data.recipe);
 }
 
-export async function deleteRecipe(id: string): Promise<void> {
-    const url = buildUrl(`${RECIPES_API}/${id}`);
+export async function deleteRecipeDraft(id: string): Promise<void> {
+    const url = buildUrl(`${RECIPES_API}/${id}/draft`);
     const options = {
         method: "DELETE",
+    };
+
+    return fetch(url, options)
+        .then(response => {
+            if (!response.ok) throw new Error(response.statusText);
+        });
+}
+
+export async function publishRecipeDraft(id: string): Promise<void> {
+    const url = buildUrl(`${RECIPES_API}/${id}/draft/preview`);
+    const options = {
+        method: "PATCH",
     };
 
     return fetch(url, options)

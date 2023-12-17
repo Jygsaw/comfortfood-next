c01860abe776758000cfb1274b50c0d51e78473f
Justin Cheung <jygsaw@gmail.com>
2023-12-17 03:46:52 -0800

    [CHG] Update database definition and connection

--- app/_lib/db.ts
+++ app/_lib/db.ts
@@ -1,5 +1,5 @@
 /*
-    = TEMPORARY LOCATION FOR DB NOTES UNTIL DOCS OR SCRIPTS AUTOMATE THE PROCESS =
+    = TEMPORARY LOCATION FOR DB NOTES UNTIL DOCS WRITTEN OR PROCESS AUTOMATED =
 
     CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
@@ -7,7 +7,7 @@
         content_id UUID DEFAULT uuid_generate_v4(),
 
         created_by UUID,
-        created_at TIMESTAMPTZ,
+        created_at TIMESTAMPTZ DEFAULT NOW(),
         updated_by UUID,
         updated_at TIMESTAMPTZ,
         draft_of UUID,
@@ -22,15 +22,37 @@
 
         PRIMARY KEY (content_id)
     );
+
+    CREATE OR REPLACE FUNCTION update_updated_column()
+    RETURNS TRIGGER AS $$
+    BEGIN
+        NEW.updated_at = NOW();
+        RETURN NEW;
+    END;
+    $$ LANGUAGE 'plpgsql';
+
+    CREATE TRIGGER update_contents_updated
+    BEFORE UPDATE ON contents
+    FOR EACH ROW EXECUTE PROCEDURE update_updated_column();
 */
 
 import postgres from "postgres";
+
+export const transformNull = (record: Record<string, any>) => {
+    for (const key in record) {
+        if (record[key] === null) {
+            record[key] = "";
+        }
+    }
+    return record;
+};
 
 const sql = postgres({
     host: process.env.POSTGRES_HOSTNAME,
     database: process.env.POSTGRES_DATABASE,
     username: process.env.POSTGRES_USERNAME,
     password: process.env.POSTGRES_PASSWORD,
+    transform: postgres.camel,
 });
 
 export default sql;

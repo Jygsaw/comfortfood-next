c105b6d1cafaf1e4f7d071445c3927ce9625d4e7
Justin Cheung <jygsaw@gmail.com>
2023-12-18 04:03:12 -0800

    [CHG] Add mock article fallback during dev

--- app/(main)/articles/[id]/[[...slug]]/page.tsx
+++ app/(main)/articles/[id]/[[...slug]]/page.tsx
@@ -6,6 +6,9 @@ import EditArticleButton from "./EditArticleButton";
 
 import type { DynamicRoute } from "app/_types/site";
 
+// TODO: remove mock data fallback after dev complete
+import { MOCK_ARTICLE } from "app/_lib/mockArticle";
+
 const Page = async ({ params: { id } }: DynamicRoute) => {
     const article = await getArticle(id);
 
@@ -18,13 +21,13 @@ const Page = async ({ params: { id } }: DynamicRoute) => {
                 <h1 className="mb-2 text-4xl">{article.name}</h1>
                 <p className="mb-4 text-2xl text-gray-600">{article.description}</p>
                 <div className="my-4">
-                    <p className="text-small">By {article.createdBy}</p>
+                    <p className="text-small">By {article.createdBy ?? MOCK_ARTICLE.createdBy}</p>
                     <p className="text-small">Updated {article.updatedAt}</p>
                 </div>
                 <div className="relative w-full aspect-[1.5]">
                     <Image src={article.imageLink} alt="" fill sizes="33vw" style={{ objectFit: "contain" }} />
                 </div>
-                <div className="my-6" dangerouslySetInnerHTML={{ __html: article.content }} />
+                <div className="my-6" dangerouslySetInnerHTML={{ __html: article.content ?? MOCK_ARTICLE.content }} />
             </article>
 
             <pre>{JSON.stringify(article, null, 4)}</pre>
--- app/(main)/recipes/[id]/[[...slug]]/page.tsx
+++ app/(main)/recipes/[id]/[[...slug]]/page.tsx
@@ -6,6 +6,9 @@ import EditRecipeButton from "./EditRecipeButton";
 
 import type { DynamicRoute } from "app/_types/site";
 
+// TODO: remove mock data fallback after dev complete
+import { MOCK_RECIPE } from "app/_lib/mockRecipe";
+
 const Page = async ({ params: { id } }: DynamicRoute) => {
     const recipe = await getRecipe(id);
 
@@ -18,13 +21,13 @@ const Page = async ({ params: { id } }: DynamicRoute) => {
                 <h1 className="mb-2 text-4xl">{recipe.name}</h1>
                 <p className="mb-4 text-2xl text-gray-600">{recipe.description}</p>
                 <div className="my-4">
-                    <p className="text-small">By {recipe.createdBy}</p>
+                    <p className="text-small">By {recipe.createdBy ?? MOCK_RECIPE.createdBy}</p>
                     <p className="text-small">Updated {recipe.updatedAt}</p>
                 </div>
                 <div className="relative w-full aspect-[1.5]">
                     <Image src={recipe.imageLink} alt="" fill sizes="33vw" style={{ objectFit: "contain" }} />
                 </div>
-                <div className="my-6" dangerouslySetInnerHTML={{ __html: recipe.content }} />
+                <div className="my-6" dangerouslySetInnerHTML={{ __html: recipe.content ?? MOCK_RECIPE.content }} />
             </article>
 
             <pre>{JSON.stringify(recipe, null, 4)}</pre>

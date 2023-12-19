a1a117aa6aed27bba2a76c6737292353da7f8978
Justin Cheung <jygsaw@gmail.com>
2023-12-19 05:00:47 -0800

    [NEW] Create lists endpoint

---
+++ app/api/lists/route.ts
@@ -0,0 +1,23 @@
+import sql from "app/_lib/db";
+import { RESPONSES } from "app/api/_lib/routeUtils";
+
+import type { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+    try {
+        const params = Object.fromEntries(request.nextUrl.searchParams);
+
+        const select = await sql`
+            SELECT *
+            FROM contents
+            WHERE
+                name ILIKE ${`%${params.q ?? ""}%`}
+                ${params.type ? sql`AND type = ${params.type}` : sql``}
+            ${params.limit ? sql`LIMIT ${params.limit}` : sql``}
+        `;
+
+        return Response.json({ data: { list: select } });
+    } catch (error) {
+        return RESPONSES.SERVER_ERROR;
+    }
+}

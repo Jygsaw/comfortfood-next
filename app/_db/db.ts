a9942cf33b170ef55fc6325e48a9d07a46856da9
Justin Cheung <jygsaw@gmail.com>
2023-12-21 00:34:49 -0800

    [CHG] Update db lib with pg pool initialization

--- app/_db/db.ts
+++ app/_db/db.ts
@@ -1,13 +1,26 @@
-// TODO: migrate code from postgres to pg, since pg is requried dependency of next-auth db adapter
+// TODO: investigate using pg as db client to reduce number of dependencies,
+//       since pg is already a requried dependency of next-auth db adapter
 
 import postgres from "postgres";
+import { Pool } from "pg";
 
-const sql = postgres({
+export const sql = postgres({
     host: process.env.POSTGRES_HOSTNAME,
     database: process.env.POSTGRES_DATABASE,
     username: process.env.POSTGRES_USERNAME,
     password: process.env.POSTGRES_PASSWORD,
     transform: postgres.camel,
+});
+
+export const pool = new Pool({
+    host: process.env.POSTGRES_HOSTNAME,
+    port: 5432,
+    database: process.env.POSTGRES_DATABASE,
+    user: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
 });
 
 export default sql;

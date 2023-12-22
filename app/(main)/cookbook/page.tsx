9863af4e32ba0fc9833be0c61ea06fef5f42691d
Justin Cheung <jygsaw@gmail.com>
2023-12-21 23:07:33 -0800

    [CHG] Require login for content creation

--- app/(main)/cookbook/page.tsx
+++ app/(main)/cookbook/page.tsx
@@ -1,17 +1,29 @@
 import React from "react";
+import { getAuth } from "app/_lib/auth";
 import SectionHeader from "app/_components/SectionHeader";
-import UnderConstruction from "app/_components/UnderConstruction";
+import LoginControl from "app/_components/LoginControl";
 import AddArticleButton from "../cookbook/AddArticleButton";
 import AddRecipeButton from "../cookbook/AddRecipeButton";
+import UnderConstruction from "app/_components/UnderConstruction";
 
 const BANNER_LINK = "https://www.innatfollybeach.com/wp-content/uploads/colossal-thanksgiving-2017-header-1200x526.jpg";
 
-const Page = () => {
+const Page = async () => {
+    const session = await getAuth();
+
     return (
         <>
             <SectionHeader title="Cookbook" bannerLink={BANNER_LINK} />
-            <AddArticleButton />
-            <AddRecipeButton />
+            {!session ? (
+                <>
+                    <LoginControl type="link" /> to share your ideas and record your recipes!
+                </>
+            ) : (
+                <>
+                    <AddArticleButton />
+                    <AddRecipeButton />
+                </>
+            )}
             <UnderConstruction />
         </>
     );

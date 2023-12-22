1b1065a7b1a44ca3a712596334d0bbf536e5422d
Justin Cheung <jygsaw@gmail.com>
2023-12-22 01:31:34 -0800

    [RFG] Add more http error codes to endpoint responses list

--- app/api/_lib/routeUtils.ts
+++ app/api/_lib/routeUtils.ts
@@ -1,9 +1,14 @@
 export const RESPONSES = {
-    NO_CONTENT: new Response(null, { status: 204 }),
-    INVALID_DATA:
-        Response.json({ error: { code: 422, message: "Invalid data" } }, { status: 422 }),
+    NO_CONTENT:
+        new Response(null, { status: 204 }),
+    UNAUTHORIZED:
+        new Response(null, { status: 401 }),
+    FORBIDDEN:
+        new Response(null, { status: 403 }),
     NOT_FOUND:
         Response.json({ error: { code: 404, message: "Not found" } }, { status: 404 }),
+    INVALID_DATA:
+        Response.json({ error: { code: 422, message: "Invalid data" } }, { status: 422 }),
     SERVER_ERROR:
         Response.json({ error: { code: 500, message: "Server error" } }, { status: 500 }),
 } as const;

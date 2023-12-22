080061534290dff9c860d2d76c1502d1a2137615
Justin Cheung <jygsaw@gmail.com>
2023-12-21 23:24:13 -0800

    [CHG] Merge homepage layout to root

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,20 +1,10 @@
 import React from "react";
-import SiteHeader from "app/_components/SiteHeader";
-import SiteFooter from "app/_components/SiteFooter";
 import type { PropsWithChildren } from "react";
 
 const Layout = ({ children }: PropsWithChildren) => {
     return (
-        <div className="min-h-screen flex flex-col">
-            <header className="sticky top-0 h-24 z-30 border-b border-slate-500">
-                <SiteHeader />
-            </header>
-            <main className="container mx-auto flex-grow">
-                {children}
-            </main>
-            <footer className="border-t border-slate-500">
-                <SiteFooter />
-            </footer>
+        <div className="container mx-auto">
+            {children}
         </div>
     );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import NextAuthProvider from "app/_components/NextAuthProvider";
+import SiteHeader from "app/_components/SiteHeader";
+import SiteFooter from "app/_components/SiteFooter";
 import "./globals.css";
 
 import type { PropsWithChildren } from "react";
@@ -8,7 +10,17 @@ const RootLayout = ({ children }: PropsWithChildren) => (
     <html lang="en">
         <body className="overflow-y-scroll">
             <NextAuthProvider>
-                {children}
+                <div className="min-h-screen flex flex-col">
+                    <header className="sticky top-0 h-24 z-30 border-b border-slate-500">
+                        <SiteHeader />
+                    </header>
+                    <main className="w-full flex-grow">
+                        {children}
+                    </main>
+                    <footer className="border-t border-slate-500">
+                        <SiteFooter />
+                    </footer>
+                </div>
             </NextAuthProvider>
         </body>
     </html>

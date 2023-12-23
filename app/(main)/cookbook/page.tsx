e17bb2b7f76c81aa962f3b14c4d352dfab24c807
Justin Cheung <jygsaw@gmail.com>
2023-12-23 06:46:23 -0800

    [CHG] List authored content on cookbook page

--- app/(main)/cookbook/page.tsx
+++ app/(main)/cookbook/page.tsx
@@ -1,30 +1,43 @@
 import React from "react";
 import { getAuth } from "app/_lib/auth";
+import { getList } from "app/_lib/listsAPI";
 import SectionHeader from "app/_components/SectionHeader";
 import LoginControl from "app/_components/LoginControl";
-import AddArticleButton from "../cookbook/AddArticleButton";
-import AddRecipeButton from "../cookbook/AddRecipeButton";
-import UnderConstruction from "app/_components/UnderConstruction";
+import Card from "app/_components/Card";
+import AddArticleButton from "./AddArticleButton";
+import AddRecipeButton from "./AddRecipeButton";
 
 const BANNER_LINK = "https://www.innatfollybeach.com/wp-content/uploads/colossal-thanksgiving-2017-header-1200x526.jpg";
 
 const Page = async () => {
     const session = await getAuth();
+    const list = session ? await getList({ createdBy: session.user.userId }) : [];
 
     return (
         <>
             <SectionHeader title="Cookbook" bannerLink={BANNER_LINK} />
-            {!session ? (
-                <>
-                    <LoginControl type="link" /> to share your ideas and record your recipes!
-                </>
-            ) : (
-                <>
-                    <AddArticleButton />
-                    <AddRecipeButton />
-                </>
+
+            <nav>
+                {!session ? (
+                    <>
+                        <LoginControl type="link" /> to share your ideas and record your recipes!
+                    </>
+                ) : (
+                    <>
+                        <AddArticleButton />
+                        <AddRecipeButton />
+                    </>
+                )}
+            </nav>
+
+            {session && (
+                <section>
+                    <SectionHeader title="My Creations" />
+                    <div className="w-full grid grid-cols-3 gap-10">
+                        {list.map(data => <Card key={data.contentId} data={data} />)}
+                    </div>
+                </section>
             )}
-            <UnderConstruction />
         </>
     );
 };

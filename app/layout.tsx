541d8ea1bc185b7aa48e93700d8aba5f8bf117b3
Justin Cheung <jygsaw@gmail.com>
2023-12-26 14:13:29 -0800

    [RFG] Deprecate unused client-side session context provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import NextAuthProvider from "app/_components/NextAuthProvider";
 import SiteHeader from "app/_components/SiteHeader";
 import SiteFooter from "app/_components/SiteFooter";
 import "./globals.css";
@@ -9,19 +8,17 @@ import type { PropsWithChildren } from "react";
 const RootLayout = ({ children }: PropsWithChildren) => (
     <html lang="en">
         <body className="overflow-y-scroll">
-            <NextAuthProvider>
-                <div className="min-h-screen flex flex-col">
-                    <header className="sticky top-0 h-24 z-30 border-b border-slate-500">
-                        <SiteHeader />
-                    </header>
-                    <main className="w-full flex-grow">
-                        {children}
-                    </main>
-                    <footer className="border-t border-slate-500">
-                        <SiteFooter />
-                    </footer>
-                </div>
-            </NextAuthProvider>
+            <div className="min-h-screen flex flex-col">
+                <header className="sticky top-0 h-24 z-30 border-b border-slate-500">
+                    <SiteHeader />
+                </header>
+                <main className="w-full flex-grow">
+                    {children}
+                </main>
+                <footer className="border-t border-slate-500">
+                    <SiteFooter />
+                </footer>
+            </div>
         </body>
     </html>
 );

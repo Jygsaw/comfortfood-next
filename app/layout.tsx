c55a8c0cf2a0ea0bdef674b2069957ff19467254
Justin Cheung <jygsaw@gmail.com>
2023-12-27 07:04:49 -0800

    [RFG] Update root layout grow property

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const RootLayout = ({ children }: PropsWithChildren) => (
                 <header className="sticky top-0 h-24 z-30 border-b border-slate-500">
                     <SiteHeader />
                 </header>
-                <main className="w-full flex-grow">
+                <main className="w-full basis-0 flex-grow">
                     {children}
                 </main>
                 <footer className="border-t border-slate-500">
